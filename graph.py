04fef6c48d974f05172dec77bf1c0981b8ad211d
matty <odonnell004@gmail.com>
2019-11-19 00:43:02 -0500

    crazy ideas

--- graph.py
+++ graph.py
@@ -1,58 +1,88 @@
 from collections import namedtuple
+from functools import reduce
+from itertools import tee
 
 Line = namedtuple('Line', ['start', 'end'], verbose=True)
 Point = namedtuple('Point', ['x', 'y'], verbose=True)
 Tick = namedtuple('Tick', ['line', 'text'], verbose=True)
 
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
 def Struct(**kwargs):
     return namedtuple('Struct', ' '.join(kwargs.keys()))(**kwargs)
 
 class Viewport(object):
-    def __init__(self, height, width, padding=0):
-        self.height=height
-        self.width=width
+    def __init__(self, *axis, padding=0):
+        self.axis=axis
         self.padding=padding
 
     @property
     def drawable(self):
-        return Struct(
-                    width   = self.width-(self.padding*2),
-                    height  = self.height - (self.padding*2),
-                    left    = self.padding,
-                    right   = self.width - padding,
-                    top     = height-padding,
-                    bottom  = padding,
-                )
+        return Struct(**reduce(lambda d1, d2: {**d1, **d2}, [a.cls.drawable_info(a.size, self.padding) for a in self.axis ]))
 
-def AxisBase(object):
-    def set(self, minimum=None, maximum=None, step=None, collection=collection):
+class AxisBase(object):
+    def __repr__(self):
+        return self.__class__.__name__
+    def __init__(self, name):
+        self.name = name
+    def set(self, minimum=None, maximum=None, step=None, collection=None):
         self.min = minimum
         self.max = maximum
         self.step = step
+        self.collection=collection
 
-def X(AxisBase):
-    pass
-def Y(AxisBase):
-    pass
+    @classmethod
+    def size(cls, num, inverse=False):
+        return Struct(cls=cls, size=num)
 
-def Property(object):
-    def __init__(name, parse=lambda d: d, covert=lambda d: d):
+class X(AxisBase):
+    @classmethod
+    def drawable_info(self, size, padding):
+        return {
+                'width' : size - (padding*2),
+                'left' : padding,
+                'right': size - padding,
+                }
+
+    def scale(drawable_info, num):
+        #return lambda mi, ma: scale(num, [mi, ma], [drawable_info.left, drawable_info.right])
+        return [drawable_info.left, drawable_info.right]
+
+class Y(AxisBase):
+    @classmethod
+    def drawable_info(self, size, padding):
+        return {
+                'height': size - (padding*2),
+                'top'   : padding,
+                'bottom': size - padding,
+                }
+
+    def scale(drawable_info):
+        return [drawable_info.top, drawable_info.bottom]
+
+class Property(object):
+    def __init__(self, name, key_or_index, parse=lambda d: d, convert=lambda d: int(d)):
         self.name = name
+        self.key_or_index = key_or_index
         self.parse = parse
         self.convert = convert
 
-    def value(item):
-        return parse(item)
+    def value(self, item):
+        return self.parse(item[self.key_or_index])
 
-    def graphable(item):
-        return convert(parse(item))
+    def graphable(self, item):
+        return self.convert(parse(item[key_or_index]))
 
-def MetaInfo(object):
+class MetaInfo(object):
     def __init__(self, data, prop):
         self.min = min([prop.value(d) for d in data])
         self.max = max([prop.value(d) for d in data])
-        self.difference = max - min
-        self.type = next(set([type(prop.get(d)) for d in data]), None)
+        self.difference = self.max - self.min
+        self.type = next(iter(set(type(prop.value(d)) for d in data)), None)
 
 class MetaProxy(object):
     def __init__(self, data_collection):
@@ -60,10 +90,10 @@ class MetaProxy(object):
 
     def __getattr__(self, attr):
         prop = next(filter(lambda p: p.name == attr, self.data_collection.properties), None)
-        return MetaInfo(self.DataCollection.data, prop)
+        return MetaInfo(self.data_collection.data, prop)
 
 class DataCollection(object):
-    def __init__(self, data, properties=None):
+    def __init__(self, data, *properties):
         self.data = data
         self.properties = properties
 
@@ -71,11 +101,18 @@ class DataCollection(object):
     def meta(self):
         return MetaProxy(self)
 
+    def to_dict(self):
+        return [ { p.name: p.value(d) for p in self.properties } for d in self.data  ]
+
+    def get_property(self, name):
+        return next(filter(lambda p: p.name == name, self.properties), None)
+
 class Graph(object):
     def __init__(self, viewport):
         self.viewport = viewport
-        self.plot_objects = []
-        self.axis = []
+        self.plot_objects = {}
+        self.axis = {}
+        self.data_collections = {}
 
     @property
     def create(self):
@@ -83,13 +120,16 @@ class Graph(object):
 
 
 class CreateFactory(object):
-    def __inti__(self, graph):
+    def __init__(self, graph):
         self.graph = graph
 
-    def axis(self):
-        self.ax
+    def axis(self, axis, name=None):
         pass
 
-    def line(self):
-        pass
-        #self.graph.line_objects.append(
+    def line(self, data_collection, name, *axis):
+
+        self.graph.plot_objects[name] = [
+                Line(
+                    start=Struct(**{ str(a): data_collection.get_property(a.name).value(d1) for a in axis}),
+                    end=Struct(**{ str(a): data_collection.get_property(a.name).value(d2) for a in axis })
+                ) for (d1, d2) in pairwise(data_collection.data)  ]
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,5 +1,18 @@
 import unittest
 import graph
+from datetime import datetime
+from graph import Struct
+
+class ViewportTest(unittest.TestCase):
+    def struct_to_dict(self,struct):
+        return {**struct._asdict()}
+
+    def test_drawable_info(self):
+        vp = graph.Viewport(graph.Y.size(300, inverse=True), graph.X.size(800), padding=30)
+
+        self.assertEqual(
+                self.struct_to_dict(vp.drawable),
+                self.struct_to_dict(Struct(top=30, left=30, height=240, width=740, right=770, bottom=270)))
 
 class DataCollectionTest(unittest.TestCase):
     def test_get_property_max(self):
@@ -9,6 +22,16 @@ class DataCollectionTest(unittest.TestCase):
     def test_get_property_min(self):
         dc = graph.DataCollection([(0,0), (0,1), (0,10)], graph.Property('first_property', 1) )
         self.assertEqual(dc.meta.first_property.min, 0)
+
+    def test_property_hash(self):
+        dc = graph.DataCollection([(0,0), (0,1), (0,10)], graph.Property('first_property', 0), graph.Property('second_property', 1) )
+
+        self.assertEqual(
+                dc.to_dict(),
+                [{'first_property': 0, 'second_property': 0},
+                 {'first_property': 0, 'second_property': 1},
+                 {'first_property': 0, 'second_property': 10}])
+
 
 class PropertyTest(unittest.TestCase):
     def test_simple(self):
@@ -21,17 +44,17 @@ class PropertyTest(unittest.TestCase):
 
 class GraphTest(unittest.TestCase):
     def setUp(self):
-        self.g = graph.Graph(graph.Viewport(300, 800, padding=30))
+        self.g = graph.Graph(graph.Viewport(graph.Y.size(300, inverse=True), graph.X.size(800), padding=30))
 
     def test_create_line(self):
         data = [(0,0), (0,1), (0,10)]
-        dc = graph.DataCollection(data, graph.Property('month', 1), graph.Property('counts', 0) )
-        self.g.create.line(dc, name='counts', x='count', y='month')
+        dc = graph.DataCollection(data, graph.Property('month', 1), graph.Property('count', 0) )
+        self.g.create.line(dc,'counts', graph.X('count'), graph.Y('month'))
 
-        self.assertEqual(self.g.plot_objects['counts'], [Point, Point, Point, Line, Line ])
+        self.assertEqual(self.g.plot_objects['counts'], [])
 
     def test_create_x_axis(self):
-        self.g.create.axis(X, name='counts', x='count', y='month')
+        self.g.create.axis(graph.X, name='counts')
 
 
-        self.assertEqual(self.g.axis[X], [Line, Tick])
+        self.assertEqual(self.g.axis[graph.X], [Line, Tick])
